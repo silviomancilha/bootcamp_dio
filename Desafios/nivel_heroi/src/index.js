c44717292ba885c20999f0136eab1e1173d172bf
silviomancilha <mancilha.silvio@gmail.com>
2024-11-21 13:20:29 -0300

    codificação da logica

---
+++ Desafios/nivel_heroi/src/index.js
@@ -0,0 +1,19 @@
+let xp = 2000;
+
+if (xp <= 1000) {
+  console.log("Ferro");
+} else if (xp >= 1001 && xp <= 2000) {
+  console.log("Bronze");
+} else if (xp >= 2001 && xp <= 5000) {
+  console.log("Prata");
+} else if (xp >= 5001 && xp <= 7000) {
+  console.log("Ouro");
+} else if (xp >= 7001 && xp <= 8000) {
+  console.log("Platina");
+} else if (xp >= 8001 && xp <= 9000) {
+  console.log("Ascendente");
+} else if (xp >= 9001 && xp <= 10000) {
+  console.log("Imortal");
+} else {
+  console.log("Radiante");
+}
